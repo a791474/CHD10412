a4462ae41d55f169086d00db5aec3d5221bdc13a
a791474 <a791474@gmail.com>
2023-12-14 19:10:34 +0800

    新增jQuery link檔案、accordion的js待研究

---
+++ JS/accordion.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $(".at-title").click(function () {
+        $(this).toggleClass("active").next(".at-tab").slideToggle().parent().siblings().find(".at-tab").slideUp().prev().removeClass("active");
+        });
+    });
