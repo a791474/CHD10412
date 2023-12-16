1c70e7d4b87e6d6011bbdddb5229cbf90fb8ca93
a791474 <a791474@gmail.com>
2023-12-16 14:43:59 +0800

    完成nav滑動到多少顯示或消失，以及各頁面的TOP按鈕的事件和CSS，RWD的版本待調整

--- JS/accordion.js
+++ JS/accordion.js
@@ -3,3 +3,22 @@ $(document).ready(function () {
         $(this).toggleClass("active").next(".at-tab").slideToggle().parent().siblings().find(".at-tab").slideUp().prev().removeClass("active");
         });
     });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let accordionItems = document.querySelectorAll('.accordion-item');
+
+//     accordionItems.forEach(function (item) {
+//         item.addEventListener('click', function () {
+//             // 切換內容的可見性
+//             let content = this.nextElementSibling;
+//             content.classList.toggle('show');
+
+//             // 關閉其他已經展開的內容
+//             accordionItems.forEach(function (otherItem) {
+//                 if (otherItem !== item) {
+//                     otherItem.nextElementSibling.classList.remove('show');
+//                 }
+//             });
+//         });
+//     });
+// });
---
+++ JS/nav-onscroll-index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var topBtn = document.getElementById("topBtn");
+
+    //用JS寫滾動事件讓TOP鍵滾到網頁高度600時顯示於畫面右下方，點了之後回到頁面最上方，且回到高於600時消失。
+
+    window.onscroll = function() {
+        if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+            topBtn.style.display = "block";
+        } else {
+            topBtn.style.display = "none";
+        }
+    };
+    topBtn.addEventListener("click", function() {
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0; 
+    });
+});
+// 用jQuery寫滾動事件讓nav滾到網頁高度600時消失，滾回600以上顯示。
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scrollPosition = $(window).scrollTop();
+
+        if (scrollPosition > 600) {
+            $('.top-header').fadeOut(300);
+        } else {
+            $('.top-header').fadeIn(300);
+        }
+    });
+});
