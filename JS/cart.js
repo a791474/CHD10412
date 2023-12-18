21b13d53f2679f03ee5e265503fbd2d6a4e0259c
a791474 <a791474@gmail.com>
2023-12-18 22:16:02 +0800

    完成PC版的購物車葉面整理以及新增、減少、刪除、計算金額的JS功能

--- JS/cart.js
+++ JS/cart.js
@@ -28,7 +28,6 @@ const productData = {
         "源自日本的巨峰窯花瓶，以其精湛的陶藝工藝與自然的靈感而聞名。每一個花瓶都是匠人用心雕琢的藝術品，展現出獨特的日式美學。瓶身造型優雅，仿如巍峨的山峰，激發著對大自然的敬畏之情。精緻的釉面呈現柔和的色澤，讓花卉在其中綻放出自然、純粹的美。每一次插花都如同在日本秋日的山川間漫遊，感受四季的變幻。"
 };
 
-
 function createProductInfoBlock(data) {
     const container = document.createElement("div");
     container.classList.add("information");
@@ -58,17 +57,15 @@ document.addEventListener("DOMContentLoaded", function () {
 function addToCart(title, price) {
     // 檢查是否有購物車資訊在 localStorage 中
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
     // 將新商品加入購物車
     cart.push({ title, price });
-
     // 將更新後的購物車資訊存回 localStorage
     localStorage.setItem('cart', JSON.stringify(cart));
-
-    // 跳轉到購物車頁面
+    // 顯示已加入購物車後跳轉到購物車頁面結帳
+    window.alert('已加入購物車');
     window.location.href = 'cart.html';
 }
-
+// ↓陣列增加商品內容
 // const products = [
 //     {
 //     title: "日式巨峰窯花瓶",
--- JS/cart2.js
+++ JS/cart2.js
@@ -1,26 +1,3 @@
-//↓用class name讓多個商品新增減少數量↓
-function init(){
-	let btnMinus = document.getElementsByClassName("btnMinus");
-	let btnPlus = document.getElementsByClassName("btnPlus");
-	// let btnMinus = document.querySelectorAll(".btnMinus"); 要用CSS的選取器去選
-
-	for(let i=0; i<btnMinus.length; i++) {
-		btnMinus[i].onclick = function(e){
-			let qtyObj = e.target.nextElementSibling;
-			let qty = parseInt(qtyObj.value);
-			if(qty>0) {
-				qtyObj.value = qty -1
-			}
-		}
-		btnPlus[i].onclick = function(e){
-			let qtyObj = e.target.previousElementSibling;
-			let qty = parseInt(qtyObj.value);
-				qtyObj.value = qty +1;
-			}
-	}
-}
-window.addEventListener("load", init, false);
-
 //add
 var adds = document.getElementsByClassName("btnPlus");
 //循環
@@ -38,7 +15,7 @@ for (var i = 0; i < adds.length; i++) {
         //1.數量
         var number = parseInt(inputEle.value);
         //2.單價
-        var commElement = this.closest('.item').querySelector(".qty");
+        var commElement = this.closest('.cart-detail').querySelector(".comm");
         if (commElement !== null) {
             var price = parseFloat(commElement.textContent);
 
@@ -46,7 +23,7 @@ for (var i = 0; i < adds.length; i++) {
             var sum = number * price;
 
             //4.賦值 保留2位小數點->toFixed(2)
-            this.closest('.item').querySelector(".total").textContent = sum;
+            this.closest('.cart-detail').querySelector(".total").textContent = sum;
 
             //調用方法
             getAmount();
@@ -57,7 +34,7 @@ for (var i = 0; i < adds.length; i++) {
 }
 
 //minus
-var minus = document.getElementsByClassName("minus");
+var minus = document.getElementsByClassName("btnMinus");
 //循環
 for (var i = 0; i < minus.length; i++) {
     //單擊事件
@@ -77,7 +54,7 @@ for (var i = 0; i < minus.length; i++) {
             //1.數量
             var number = parseInt(inputEle.value);
             //2.單價
-            var commElement = this.closest('.item').querySelector(".comm");
+            var commElement = this.closest('.cart-detail').querySelector(".comm");
             if (commElement !== null) {
                 var price = parseFloat(commElement.textContent);
 
@@ -85,7 +62,7 @@ for (var i = 0; i < minus.length; i++) {
                 var sum = number * price;
 
                 //4.賦值 保留2位小數點->toFixed(2)
-                this.closest('.item').querySelector(".total").textContent = sum;
+                this.closest('.cart-detail').querySelector(".total").textContent = sum;
 
                 //調用方法
                 getAmount();
@@ -96,15 +73,13 @@ for (var i = 0; i < minus.length; i++) {
     }
 }
 
-
-
 //刪除品項
 var removeItems = document.getElementsByClassName("remove");
 //循環
 for (var i = 0; i < removeItems.length; i++) {
     //點擊事件
     removeItems[i].onclick = function () {
-        var itemToRemove = this.closest('.item');
+        var itemToRemove = this.closest('.cart-item');
         if (itemToRemove !== null) {
             itemToRemove.remove();
             // 調用方法重新計算金額
@@ -113,4 +88,42 @@ for (var i = 0; i < removeItems.length; i++) {
             // console.error("Item to remove not found!");
         }
     }
+}
+
+//計算金額
+function getAmount() {
+    //0.累計和
+    var sum = 0;
+    //1.total
+    var totals = document.getElementsByClassName("total");
+    //2.循環
+    for (var i = 0; i < totals.length; i++) {
+        sum += parseFloat(totals[i].textContent);
+    }
+
+    //3.賦值
+    document.getElementById("sum2").textContent = sum;
+    // document.getElementById("sum3").textContent = sum - discount.textContent + shipping.textContent;
+
+
+// 設定折扣和運費
+var shipping = document.getElementById("shipping");
+
+var itemCount = document.querySelectorAll('.cart-inputs input[type="text"]');
+var totalItems = 0;
+
+for (var j = 0; j < itemCount.length; j++) {
+    totalItems += parseInt(itemCount[j].value);
+}
+
+var shippingCost = 100; // 運費金額
+var orderTotal = document.getElementById("sum3");
+
+if (totalItems === 0) {
+    shipping.innerHTML = 0;
+    orderTotal.innerHTML = 0; // 將總金額設為零
+} else {
+    shipping.innerHTML = shippingCost;
+    orderTotal.innerHTML = sum  + shippingCost;
+}
 }
