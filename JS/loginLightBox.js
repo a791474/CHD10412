921bb735a5bde6a1d39c8d48e736912763fe49d5
a791474 <a791474@gmail.com>
2023-12-18 12:37:35 +0800

    修改全部頁面的登入燈箱，修改燈箱內的CSS樣式、新增未輸入帳號密碼判斷JS

--- JS/loginLightBox.js
+++ JS/loginLightBox.js
@@ -19,15 +19,29 @@ function closeModal() {
 function login() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
 
-    if (username === 'Yee' && password === 'a12345') {
-        // 登入成功的處理邏輯
-        alert(`歡迎回來，${username}！`);
-        usernameDisplay.textContent = username;
-        closeModal();
+    // 清空錯誤訊息
+    errorMessage.textContent = '';
+
+    // 進行輸入內容的判斷
+    if (username === '' && password === '') {
+        errorMessage.textContent = '請輸入登入資訊';
+    } else if (username === '') {
+        errorMessage.textContent = '請輸入帳號';
+    } else if (password === '') {
+        errorMessage.textContent = '請輸入密碼';
     } else {
-        // 登入失敗的處理邏輯
-        alert('登入失敗，將跳轉至註冊頁面。');
-        window.location.href = "register.html";
+        // 如果有輸入帳號和密碼，進行登入判斷
+        if (username === 'Yee' && password === 'a12345') {
+            // 登入成功的處理邏輯
+            alert(`歡迎回來，${username}！`);
+            usernameDisplay.textContent = username;
+            closeModal();
+        } else {
+            // 登入失敗的處理邏輯
+            alert('登入失敗，將跳轉至註冊頁面。');
+            window.location.href = "register.html";
+        }
     }
 }
--- JS/nav-onscroll-user.js
+++ JS/nav-onscroll-user.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     var topBtn = document.getElementById("topBtn");
 
-    //用JS寫滾動事件讓TOP鍵滾到網頁高度600時顯示於畫面右下方，點了之後回到頁面最上方，且回到高於600時消失。
-
     window.onscroll = function() {
-        if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+        if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
             topBtn.style.display = "block";
         } else {
             topBtn.style.display = "none";
@@ -15,12 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
         document.documentElement.scrollTop = 0; 
     });
 });
-// 用jQuery寫滾動事件讓nav滾到網頁高度600時消失，滾回600以上顯示。
+
 $(document).ready(function () {
     $(window).scroll(function () {
         var scrollPosition = $(window).scrollTop();
 
-        if (scrollPosition > 600) {
+        if (scrollPosition > 5) {
             $('.top-header').fadeOut(300);
         } else {
             $('.top-header').fadeIn(300);
