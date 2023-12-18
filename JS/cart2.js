535cc5e2e9e20357cf051351719740c51bb40d87
a791474 <a791474@gmail.com>
2023-12-19 01:54:06 +0800

    新增刪除商品時判斷，若購物車無商品則顯示空購物車div

--- JS/cart2.js
+++ JS/cart2.js
@@ -1,8 +1,6 @@
-//add
+//商品新增
 var adds = document.getElementsByClassName("btnPlus");
-//循環
 for (var i = 0; i < adds.length; i++) {
-    //單擊事件
     adds[i].onclick = function () {
         //1.獲取輸入框對象
         var inputEle = this.previousElementSibling;
@@ -10,7 +8,6 @@ for (var i = 0; i < adds.length; i++) {
         var inputValue = inputEle.value;
         //3.重新複製
         inputEle.value = parseInt(inputValue) + 1;
-
         //計算金額
         //1.數量
         var number = parseInt(inputEle.value);
@@ -18,26 +15,20 @@ for (var i = 0; i < adds.length; i++) {
         var commElement = this.closest('.cart-detail').querySelector(".comm");
         if (commElement !== null) {
             var price = parseFloat(commElement.textContent);
-
             //3.金額
             var sum = number * price;
-
             //4.賦值 保留2位小數點->toFixed(2)
             this.closest('.cart-detail').querySelector(".total").textContent = sum;
-
-            //調用方法
             getAmount();
         } else {
-            console.error(".comm element not found!");
+            // console.error(".comm element not found!");
         }
     }
 }
 
-//minus
+//商品減少
 var minus = document.getElementsByClassName("btnMinus");
-//循環
 for (var i = 0; i < minus.length; i++) {
-    //單擊事件
     minus[i].onclick = function () {
         //1.獲取輸入框對象
         var inputEle = this.nextElementSibling;
@@ -48,42 +39,42 @@ for (var i = 0; i < minus.length; i++) {
         if (parseInt(inputValue) > 1) {
             //3.重新複製
             inputEle.value = parseInt(inputValue) - 1;
-
-
-            //計算金額
+            //加上計算金額
             //1.數量
             var number = parseInt(inputEle.value);
             //2.單價
             var commElement = this.closest('.cart-detail').querySelector(".comm");
             if (commElement !== null) {
                 var price = parseFloat(commElement.textContent);
-
                 //3.金額
                 var sum = number * price;
-
                 //4.賦值 保留2位小數點->toFixed(2)
                 this.closest('.cart-detail').querySelector(".total").textContent = sum;
-
-                //調用方法
                 getAmount();
             } else {
-                console.error(".comm element not found!");
+                // console.error(".comm element not found!");
             }
         }
     }
 }
 
-//刪除品項
 var removeItems = document.getElementsByClassName("remove");
-//循環
 for (var i = 0; i < removeItems.length; i++) {
-    //點擊事件
     removeItems[i].onclick = function () {
         var itemToRemove = this.closest('.cart-item');
         if (itemToRemove !== null) {
             itemToRemove.remove();
             // 調用方法重新計算金額
             getAmount();
+
+            // 檢查是否還有商品存在
+            var cartItems = document.querySelectorAll('.cart-item');
+            if (cartItems.length === 0) {
+                document.getElementById('cartContainer').style.display = 'none';
+                document.querySelector('.col-3').style.display = 'none';
+                document.getElementById('col-9Title').style.display = 'none';
+                document.getElementById('emptyCartContainer').style.display = 'flex';
+            }
         } else {
             // console.error("Item to remove not found!");
         }
@@ -92,38 +83,31 @@ for (var i = 0; i < removeItems.length; i++) {
 
 //計算金額
 function getAmount() {
-    //0.累計和
     var sum = 0;
-    //1.total
     var totals = document.getElementsByClassName("total");
-    //2.循環
     for (var i = 0; i < totals.length; i++) {
         sum += parseFloat(totals[i].textContent);
     }
-
-    //3.賦值
     document.getElementById("sum2").textContent = sum;
-    // document.getElementById("sum3").textContent = sum - discount.textContent + shipping.textContent;
 
+// 設定運費
+    var shipping = document.getElementById("shipping");
 
-// 設定折扣和運費
-var shipping = document.getElementById("shipping");
+    var itemCount = document.querySelectorAll('.cart-inputs input[type="text"]');
+    var totalItems = 0;
 
-var itemCount = document.querySelectorAll('.cart-inputs input[type="text"]');
-var totalItems = 0;
+    for (var j = 0; j < itemCount.length; j++) {
+        totalItems += parseInt(itemCount[j].value);
+    }
 
-for (var j = 0; j < itemCount.length; j++) {
-    totalItems += parseInt(itemCount[j].value);
-}
+    var shippingCost = 100; // 金額
+    var orderTotal = document.getElementById("sum3");
 
-var shippingCost = 100; // 運費金額
-var orderTotal = document.getElementById("sum3");
-
-if (totalItems === 0) {
-    shipping.innerHTML = 0;
-    orderTotal.innerHTML = 0; // 將總金額設為零
-} else {
-    shipping.innerHTML = shippingCost;
-    orderTotal.innerHTML = sum  + shippingCost;
-}
+    if (totalItems === 0) {
+        shipping.innerHTML = 0;
+        orderTotal.innerHTML = 0; // 將總金額設為零
+    } else {
+        shipping.innerHTML = shippingCost;
+        orderTotal.innerHTML = sum  + shippingCost;
+    }
 }
